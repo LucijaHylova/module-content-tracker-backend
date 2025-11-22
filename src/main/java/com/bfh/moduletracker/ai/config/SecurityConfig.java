3def97ad4ca82bb0e0397b5c5e0e32abe129ed20
u234808 <lucia.hylova@sbb.ch>
2025-11-22 14:31:49 +0100

    first commit

--- src/main/java/com/bfh/moduletracker/ai/config/SecurityConfig.java
+++ src/main/java/com/bfh/moduletracker/ai/config/SecurityConfig.java
@@ -86,7 +86,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers(HttpMethod.GET, "/", "/error").permitAll()
 //                        .requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/api-docs/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**","/api/docs", "/api/docs/**" ).permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**","/api/docs", "/api/docs/**" ).permitAll()
                         .requestMatchers(HttpMethod.GET, "/ai/**", "/modules/all", "/content/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/authenticate").permitAll()
                         .requestMatchers(HttpMethod.POST, "test/reset", "vectorStore/load","/fakerUserGenerator/generate").permitAll()
