ef89447f119771ae5c4433fa6b4cf51ee79e4888
u234808 <lucia.hylova@sbb.ch>
2025-11-22 20:37:25 +0100

    first commit

--- src/main/java/com/bfh/moduletracker/ai/config/SecurityConfig.java
+++ src/main/java/com/bfh/moduletracker/ai/config/SecurityConfig.java
@@ -92,7 +92,9 @@ public class SecurityConfig {
                                         "/swagger-ui.html",
                                         "/swagger-ui/**",
                                         "/v3/api-docs",
-                                        "/v3/api-docs/**"
+                                        "/v3/api-docs/**",
+                                        "/api/docs",
+                                        "/api/docs/**"
                                 ).permitAll()
 
                                 .requestMatchers(HttpMethod.GET, "/ai/**", "/modules/all", "/content/**").permitAll()
