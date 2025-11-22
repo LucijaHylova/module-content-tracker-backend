223623ab20389e04637c5c56a47e1847fa9c49ed
u234808 <lucia.hylova@sbb.ch>
2025-11-22 09:37:20 +0100

    first commit

--- src/main/java/com/bfh/moduletracker/ai/service/loader/ModuleLoader.java
+++ src/main/java/com/bfh/moduletracker/ai/service/loader/ModuleLoader.java
@@ -33,11 +33,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(1)
+@Profile("local")
 public class ModuleLoader implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(ModuleLoader.class);
