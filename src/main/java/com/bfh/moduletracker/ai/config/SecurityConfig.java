35c21c989b7d0c4eff1d6c4b4d02cea08ed3a519
u234808 <lucia.hylova@sbb.ch>
2025-11-22 10:56:00 +0100

    first commit

--- src/main/java/com/bfh/moduletracker/ai/config/SecurityConfig.java
+++ src/main/java/com/bfh/moduletracker/ai/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(HttpMethod.GET, "/", "/error", "/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "/error").permitAll()
                         .requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/ai/**", "/modules/all", "/content/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/authenticate").permitAll()
